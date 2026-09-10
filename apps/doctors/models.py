79895afc868d8b38cecdbc98edb91affb62f8e7f
Mahsa-Alipour <39373424+Mahsa-Alipour@users.noreply.github.com>
2026-09-10 10:48:33 -0700

    Update models.py

--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -66,10 +66,7 @@ class Doctor(models.Model):
                 condition=models.Q(visit_fee__gte=0),
                 name="doctor_visit_fee_gte_zero",
             ),
-            models.UniqueConstraint(
-                fields=["full_name", "specialty"],
-                name="unique_doctor_per_specialty",
-            ),
+            
         ]
 
     def __str__(self):
