3f5c8360b83870fac3ec8e13adac756e3abe1305
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2026-09-12 13:43:16 +0330

    fix(settings): preserve existing settings while adding wallet app

--- config/settings/base.py
+++ config/settings/base.py
@@ -28,7 +28,6 @@ LOCAL_APPS = [
 ]
 
 THIRD_PARTY_APPS = []
-
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
@@ -42,16 +41,20 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "config.urls"
-TEMPLATES = [{"BACKEND":"django.template.backends.django.DjangoTemplates","DIRS":[BASE_DIR / "templates"],"APP_DIRS":True,"OPTIONS":{"context_processors":["django.template.context_processors.request","django.contrib.auth.context_processors.auth","django.contrib.messages.context_processors.messages"]}}]
+TEMPLATES = [{"BACKEND": "django.template.backends.django.DjangoTemplates", "DIRS": [BASE_DIR / "templates"], "APP_DIRS": True, "OPTIONS": {"context_processors": ["django.template.context_processors.request", "django.contrib.auth.context_processors.auth", "django.contrib.messages.context_processors.messages"]}}]
 WSGI_APPLICATION = "config.wsgi.application"
-DATABASES = {"default":{"ENGINE":"django.db.backends.postgresql","NAME":env("POSTGRES_DB"),"USER":env("POSTGRES_USER"),"PASSWORD":env("POSTGRES_PASSWORD"),"HOST":env("POSTGRES_HOST"),"PORT":env("POSTGRES_PORT")}}
-AUTHENTICATION_BACKENDS=["apps.accounts.backends.EmailAuthBackend","django.contrib.auth.backends.ModelBackend"]
-AUTH_USER_MODEL="accounts.User"
-LANGUAGE_CODE="fa"
-TIME_ZONE=env("TIME_ZONE",default="Asia/Tehran")
-USE_I18N=True
-USE_TZ=True
-STATIC_URL="static/"
-STATICFILES_DIRS=[BASE_DIR / "static"]
-STATIC_ROOT=BASE_DIR / "staticfiles"
-DEFAULT_AUTO_FIELD="django.db.models.BigAutoField"
+DATABASES = {"default": {"ENGINE": "django.db.backends.postgresql", "NAME": env("POSTGRES_DB"), "USER": env("POSTGRES_USER"), "PASSWORD": env("POSTGRES_PASSWORD"), "HOST": env("POSTGRES_HOST"), "PORT": env("POSTGRES_PORT")}}
+AUTHENTICATION_BACKENDS = ["apps.accounts.backends.EmailAuthBackend", "django.contrib.auth.backends.ModelBackend"]
+AUTH_USER_MODEL = "accounts.User"
+LANGUAGE_CODE = "fa"
+TIME_ZONE = env("TIME_ZONE", default="Asia/Tehran")
+USE_I18N = True
+USE_TZ = True
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+EMAIL_BACKEND = env("EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="noreply@localhost")
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
