1ed8c758bc895c6d35c91d4d4deb84ceb09fc3bf
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2026-09-12 15:41:30 +0330

    fix(accounts): enable password validation rules

--- config/settings/base.py
+++ config/settings/base.py
@@ -28,7 +28,23 @@ LOCAL_APPS = [
 ]
 
 THIRD_PARTY_APPS = []
+
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
