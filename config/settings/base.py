f2a51b6dcf4c3d1f8d1ca0c5eaa3599be545ffa0
mahsa_ap <mahsa.alipourrr1375@gmail.com>
2026-09-14 05:24:14 +0000

    chore(docker): setup compose baseline for web and db services (#19)

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,7 +15,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list(
     "ALLOWED_HOSTS",
-    default=["localhost", "127.0.0.1"],
+    default=["localhost", "127.0.0.1","192.168.96.110"],
 )
 
 
