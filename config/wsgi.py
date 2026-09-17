fa14547bfcea3ed0b9756c11957583f8848366c5
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2026-09-17 20:14:13 +0330

    chore(vercel): select preview settings on Vercel

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+settings_module = (
+    "config.settings.preview" if os.environ.get("VERCEL") else "config.settings.development"
+)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
