81fec15f9cfbd0b9ac67f7b087fa06a1429d805e
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2026-09-17 20:14:20 +0330

    chore(vercel): use preview settings for Vercel commands

--- manage.py
+++ manage.py
@@ -7,7 +7,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+    settings_module = (
+        "config.settings.preview" if os.environ.get("VERCEL") else "config.settings.development"
+    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
